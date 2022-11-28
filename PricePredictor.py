860dc55c0b6164c541a076ef312fe0d794120dd5
dawsoncarter0 <89360689+dawsoncarter0@users.noreply.github.com>
2022-11-28 12:02:09 -0600

    adding price predictor

--- PricePredictor.py
+++ PricePredictor.py
@@ -1,19 +1,132 @@
-import os
 import pandas as pd
 import numpy as np
 import math
-import datetime as dt
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score 
-from sklearn.metrics import mean_poisson_deviance, mean_gamma_deviance, accuracy_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 import matplotlib.pyplot as plt
 from itertools import cycle
-import plotly.graph_objects as go
 import plotly.express as px
-from plotly.subplots import make_subplots
 
+maindf = pd.read_csv('BTC-USD.csv')
+
+# Taking all of the closing prices
+closedf = maindf[['Date','Close']]
+
+# Plotting this
+fig = px.line(closedf, x=closedf.Date, y=closedf.Close,labels={'date':'Date','close':'Close Stock'})
+fig.update_traces(marker_line_width=2, opacity=0.8, marker_line_color='orange')
+fig.update_layout(title_text='Whole period of timeframe of Bitcoin close price 2014-2022', plot_bgcolor='white', 
+                  font_size=15, font_color='black')
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+# Taking closing prices of year 1 and plotting
+closedf = closedf[closedf['Date'] > '2021-02-19']
+close_stock = closedf.copy()
+fig = px.line(closedf, x=closedf.Date, y=closedf.Close,labels={'date':'Date','close':'Close Stock'})
+fig.update_traces(marker_line_width=2, opacity=0.8, marker_line_color='orange')
+fig.update_layout(title_text='Considered period to predict Bitcoin close price', 
+                  plot_bgcolor='white', font_size=15, font_color='black')
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+# Normalizing the data to prep, using this way preserves the shape of the original distribution
+del closedf['Date']
+scaler=MinMaxScaler(feature_range=(0,1))
+closedf=scaler.fit_transform(np.array(closedf).reshape(-1,1))
+
+# Creating the training and testing set 
+training_size=int(len(closedf)*0.60)
+test_size=len(closedf)-training_size
+train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
+
+# Transforming the close price based on time-series analysis forecasting
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+time_step = 15
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, y_test = create_dataset(test_data, time_step)
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
+
+# Building the model 
+model=Sequential()
+model.add(LSTM(10,input_shape=(None,1),activation="relu"))
+model.add(Dense(1))
+model.compile(loss="mean_squared_error",optimizer="adam")
+history = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=200,batch_size=32,verbose=1)
+
+# Plotting the training loss and the validation loss
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(loss))
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend(loc=0)
+plt.figure()
+plt.show()
+
+# Predicitng using the train and test sets
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+
+# Transforming back to original 
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+original_ytrain = scaler.inverse_transform(y_train.reshape(-1,1)) 
+original_ytest = scaler.inverse_transform(y_test.reshape(-1,1)) 
+
+# Root mean squared error, mean square error, and mean absolute error with train and test sets
+print("Train data RMSE: ", math.sqrt(mean_squared_error(original_ytrain,train_predict)))
+print("Train data MSE: ", mean_squared_error(original_ytrain,train_predict))
+print("Train data MAE: ", mean_absolute_error(original_ytrain,train_predict))
+
+print("Test data RMSE: ", math.sqrt(mean_squared_error(original_ytest,test_predict)))
+print("Test data MSE: ", mean_squared_error(original_ytest,test_predict))
+print("Test data MAE: ", mean_absolute_error(original_ytest,test_predict))
+
+# Variance regressions
+print(explained_variance_score(original_ytrain, train_predict))
+print(explained_variance_score(original_ytest, test_predict))
+
+# R squared scores
+print("Train data R2 score:", r2_score(original_ytrain, train_predict))
+print("Test data R2 score:", r2_score(original_ytest, test_predict))
+
+# Comparing original closing prices and predicted closing prices
+look_back=time_step
+trainPredictPlot = np.empty_like(closedf)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+
+testPredictPlot = np.empty_like(closedf)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(train_predict)+(look_back*2)+1:len(closedf)-1, :] = test_predict
+
+names = cycle(['Original close price','Train predicted close price','Test predicted close price'])
+plotdf = pd.DataFrame({'date': close_stock['Date'],
+                       'original_close': close_stock['Close'],
+                      'train_predicted_close': trainPredictPlot.reshape(1,-1)[0].tolist(),
+                      'test_predicted_close': testPredictPlot.reshape(1,-1)[0].tolist()})
+fig = px.line(plotdf,x=plotdf['date'], y=[plotdf['original_close'],plotdf['train_predicted_close'],
+                                          plotdf['test_predicted_close']],
+              labels={'value':'Stock price','date': 'Date'})
+fig.update_layout(title_text='Comparision between original close price vs predicted close price',
+                  plot_bgcolor='white', font_size=15, font_color='black', legend_title_text='Close Price')
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
--- README.md
+++ README.md
@@ -2,22 +2,19 @@
 This repository will make an attempt to predict Bitcoin prices using long short-term memory
 
 Libraries needed include:
-os, pandas, numpy, math, datetime, matplotlib.pyplot
+pandas, numpy, math, matplotlib.pyplot
 
 For evaluation we need:
-from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score 
-from sklearn.metrics import mean_poisson_deviance, mean_gamma_deviance, accuracy_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score
 from sklearn.preprocessing import MinMaxScaler
 
 For model building:
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
 
 For plotting:
 import matplotlib.pyplot as plt
 from itertools import cycle
-import plotly.graph_objects as go
 import plotly.express as px
-from plotly.subplots import make_subplots
